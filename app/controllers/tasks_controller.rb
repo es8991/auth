60dd6c447a2f50e7290fe2fd074929ea97deac30
Sarah El-Guindy <sarah.elguindy@kellogg.northwestern.edu>
2025-02-27 03:05:48 +0000

    class 8

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,18 +1,31 @@
 class TasksController < ApplicationController
   def index
-    @tasks = Task.all
+    if session["user_id"] != nil
+        @tasks = Task.where({"user_id" => session["user_id"]})
+
+    else 
+        flash["notice"] = "Login first!"
+        redirect_to "/login"
+    end
+
   end
+
 
   def create
     @task = Task.new
     @task["description"] = params["description"]
+    @task["user_id"] = session["user_id"]
     @task.save
     redirect_to "/tasks"
   end
 
   def destroy
     @task = Task.find_by({ "id" => params["id"] })
-    @task.destroy
+    if @task["user_id"] == session["user_id"]
+      @task.destroy
+
+    end
+    
     redirect_to "/tasks"
   end
 end
--- scripts/encryption.rb
+++ scripts/encryption.rb
@@ -4,5 +4,13 @@
 
 # TODO:
 # 1. encrypt a secret and puts encrypted string
+password = "Sarah1998!" 
+encrypted_password = BCrypt::Password.create(password)
+puts encrypted_password
+# $2a$12$SsME3O3SQZ960UlFV2bsG.YQyuu/zyHMYkPkBTdC1vixgy7fz30T2
+
+
 # 2. prepare encrypted string for testing
+puts BCrypt::Password.new(encrypted_password) == "Sarah1998!"
+
 # 3. test secret against prepared encrypted string
